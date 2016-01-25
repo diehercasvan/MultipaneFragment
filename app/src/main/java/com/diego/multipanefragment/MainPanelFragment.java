548984fda7bf20df9ae97c4126572d236dcc02d9
Diego Casallas <diehercasvan@gmail.com>
2016-01-25 15:10:59 -0500

    Multipane Fragment

---
+++ app/src/main/java/com/diego/multipanefragment/MainPanelFragment.java
@@ -0,0 +1,21 @@
+package com.diego.multipanefragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by DIEGO  CASALLAS on 25/01/2016.
+ */
+public class MainPanelFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_main_panel,container,false);
+        return view;
+    }
+}
